3bdeffbf873e0822706910f49fe7f8442d33b559
Light, Ethan G <ethangl2@illinois.edu>
2022-06-23 15:57:03 -0500

    2nd commit

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ import java.util.LinkedList;
 public class App {
     public static void main(String[] args) throws Exception {
         LinkedList<Cell> network = SimulationMethods.createCellChain(3,6.0,180.0,3000.0,2400.0,60.0);
-        System.out.println(network.getLast());
         SimulationMethods.changeCapacity(network, 0.5, 600 , 6);
-        System.out.println(network.getLast());
-        network = SimulationMethods.initializeFlow(network);
-        System.out.println(network.getLast());
+        SimulationMethods.updateFlow(network);
+        int time = 0;
+        int time_step = 6;
+
     }
 }
