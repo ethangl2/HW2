2ecc1ee0b99a0225736de6eb8bb4fb97d37fc442
Light, Ethan G <ethangl2@illinois.edu>
2022-07-01 16:32:01 -0500

    new simulation method. Needs more debugging.

--- src/SimulationMethods.java
+++ src/SimulationMethods.java
@@ -17,7 +17,6 @@ class SimulationMethods {
 
     public static void simulate(Graph<Road, DefaultEdge> network, int time) {
         int num_steps = time/timestep;
-        initializeFlow(network);
         for (int t = 1; t <= num_steps; t ++) {
             Set<Road> roadSet = network.vertexSet();
             Object[] roadArray = roadSet.toArray();
@@ -49,40 +48,81 @@ class SimulationMethods {
         }
     }
     public static void simulate2(Graph<Road, DefaultEdge> network, int time) {
-        initializeFlow(network);
         Graph<Cell,DefaultEdge> cell_network = generateCellGraph(network);
         int num_steps = time/timestep;
-        for (int t = 1; t <=1;t++) {
+        for (int t = 1; t <=num_steps;t++) {
+            double numEPrime = 0;
+            Cell source_cell = (Cell) cell_network.vertexSet().toArray()[2];
             for (Cell currentCell:cell_network.vertexSet()) {
                 if (vertexHasSuccessors(cell_network,currentCell)&&vertexHasPredecessors(cell_network,currentCell)) {
                     List<Cell> successor_list = successorListOf(cell_network,currentCell);
-                    System.out.println("Successor List " + successor_list);
                     List<Cell> predecessor_list = predecessorListOf(cell_network,currentCell);
-                    System.out.println("predeccessor List " + predecessor_list);
-                    double numEPrime = 0;
                     if (successor_list.size() > 1) {
 
                     } else if(predecessor_list.size()>1) {
-
-                    } else {
-                        Cell next_cell = successor_list.get(0);
-                        Cell prev_cell = predecessor_list.get(0);
-                        double S = Math.min(currentCell.getMaxFlow(),currentCell.getNumCars());
-                        double R = Math.min(next_cell.getMaxFlow(),next_cell.getMaxCars()-next_cell.getNumCars());
-                        double yt = Math.min(S,R);
-                        if (!vertexHasPredecessors(cell_network,prev_cell)) {
-                            currentCell.setNumCars(currentCell.getNumCars()-yt+prev_cell.getCurrentFlow());
-                            numEPrime = prev_cell.getNumCars() + yt;
+                        double pb = 0.8;
+                        double pc = 0.2;
+                        double sb = Math.min(predecessor_list.get(0).getMaxFlow(),predecessor_list.get(0).getPlaceHolder());
+                        double sc = Math.min(predecessor_list.get(1).getMaxFlow(),predecessor_list.get(1).getPlaceHolder());
+                        double r = Math.min(currentCell.getMaxFlow(),currentCell.getMaxCars()-currentCell.getNumCars());
+                        if (r >= sb + sc) {
+                            currentCell.setPlaceHolder(currentCell.getNumCars());
+                            currentCell.setNumCars(currentCell.getMaxCars() + sb + sc);
+                            predecessor_list.get(0).setNumCars(predecessor_list.get(0).getNumCars()-sb);
+                            predecessor_list.get(1).setNumCars(predecessor_list.get(1).getNumCars()-sc);
                         } else {
-                            currentCell.setNumCars(numEPrime-yt);
-                            numEPrime = next_cell.getNumCars() + yt;
+                            double yb = middleOfThree(sb,r-sc,pb*r);
+                            double yc = middleOfThree(sc,r-sb,pc*r);
+                            currentCell.setPlaceHolder(currentCell.getNumCars());
+                            currentCell.setNumCars(currentCell.getMaxCars() + yb + yc);
+                            predecessor_list.get(0).setNumCars(predecessor_list.get(0).getNumCars()-yb);
+                            predecessor_list.get(1).setNumCars(predecessor_list.get(1).getNumCars()-yc);
                         }
+                    } else {
+                        Cell prev_cell = predecessor_list.get(0);
+                        Cell next_cell = successor_list.get(0);
+                        if (!vertexHasPredecessors(cell_network,prev_cell)) {
+                            currentCell.setPlaceHolder(currentCell.getNumCars());
+                            currentCell.setNumCars(currentCell.getNumCars()+prev_cell.getCurrentFlow());
+                            continue;
+                        }
+                        double S = Math.min(prev_cell.getMaxFlow(),prev_cell.getPlaceHolder());
+                        double R = Math.min(currentCell.getMaxFlow(),currentCell.getMaxCars()-currentCell.getNumCars());
+                        double yt = Math.min(S,R);
+                        if (!vertexHasSuccessors(cell_network,next_cell)) {
+                            currentCell.setNumCars(currentCell.getNumCars()+yt- source_cell.getCurrentFlow());
+                            prev_cell.setNumCars(prev_cell.getNumCars()-yt);
+                            continue;
+                        }
+                        currentCell.setPlaceHolder(currentCell.getNumCars());
+                        currentCell.setNumCars(currentCell.getNumCars()+yt);
+                        prev_cell.setNumCars(prev_cell.getNumCars()-yt);
                     }
                 }
             }
-            //System.out.println(network + " " + t*timestep);
+            System.out.println(network + " " + t*timestep);
         }
 
+    }
+
+    public static double middleOfThree(double a, double b, double c) {
+        if (a > b) {
+            if (b > c) {
+                return b;
+            }else if (a > c) {
+                return c;
+            }else {
+                return a;
+            }
+        } else {
+            if (a > c) {
+                return a;
+            }else if (b > c) {
+                return c;
+            }else {
+                return b;
+            }
+        }
     }
     public static Graph<Cell,DefaultEdge> generateCellGraph(Graph<Road, DefaultEdge> network) {
         Graph<Cell,DefaultEdge> toreturn = new DefaultDirectedGraph<>(DefaultEdge.class);
@@ -95,7 +135,7 @@ class SimulationMethods {
                 double id = point[1];
                 Road parentRoad = getRoadByID(network,(int)id);
                 Set<Cell> cellSet = parentRoad.getRoadGraph().vertexSet();
-                int length = parentRoad.getLength();
+                double length = parentRoad.getLength();
                 Object[] cellArray = cellSet.toArray();
                 int startIndex = (int) ((cellArray.length-2)*location/length);
                 Cell cell = (Cell) cellArray[startIndex];
@@ -115,7 +155,7 @@ class SimulationMethods {
         }
         return null;
     }
-    public static void createMergingRoad(Graph<Road, DefaultEdge> network,int length,double jamDensity, double maxFlow, double currentFlow,double ffspeed, int id, int parentRode, double x) {
+    public static void createMergingRoad(Graph<Road, DefaultEdge> network,double length,double jamDensity, double maxFlow, double currentFlow,double ffspeed, int id, int parentRode, double x) {
         network.addVertex(new Road(length,jamDensity,maxFlow,currentFlow,ffspeed,id));
         Set<Road> roadSet = network.vertexSet();
         Object[] roadArray = roadSet.toArray();
@@ -128,16 +168,17 @@ class SimulationMethods {
         Cell []cells = new Cell[numCells];
         for (int i = 0; i < numCells ; i++) {
             if (i == 0) {
-                cells[i]= new Cell(maxFlow/3600.0*timestep,Double.MAX_VALUE,Double.MAX_VALUE,0);
+                cells[i]= new Cell(maxFlow/3600.0*timestep,Double.MAX_VALUE,Double.MAX_VALUE,Math.ceil(currentFlow/ffspeed*length/numCells));
                 newRoad.getRoadGraph().addVertex(cells[i]);
                 continue;
             }
             cells[i]= new Cell(maxFlow/3600.0*timestep,density/numCells*length,jamDensity*length/numCells,0.0);
             newRoad.getRoadGraph().addVertex(cells[i]);
+            newRoad.getRoadGraph().addEdge(cells[i-1],cells[i]);
         }
     }
 
-    public static void createFirstRoad(Graph<Road, DefaultEdge> network,int length,double jamDensity, double maxFlow, double currentFlow,double ffspeed) {
+    public static void createFirstRoad(Graph<Road, DefaultEdge> network,double length,double jamDensity, double maxFlow, double currentFlow,double ffspeed) {
         network.addVertex(new Road(length,jamDensity,maxFlow,currentFlow,ffspeed,1));
         Set<Road> roadSet = network.vertexSet();
         Object[] roadArray = roadSet.toArray();
@@ -147,7 +188,7 @@ class SimulationMethods {
         Cell []cells = new Cell[numCells];
         for (int i = 0; i < numCells ; i++) {
             if (i == 0) {
-                cells[i]= new Cell(maxFlow/3600.0*timestep,Double.MAX_VALUE,Double.MAX_VALUE,0);
+                cells[i]= new Cell(maxFlow/3600.0*timestep,Double.MAX_VALUE,Double.MAX_VALUE,Math.ceil(currentFlow/ffspeed*length/numCells));
                 newRoad.getRoadGraph().addVertex(cells[i]);
                 continue;
             } else if (i == numCells - 1) {
@@ -179,32 +220,11 @@ class SimulationMethods {
         Object[] roadArray = roadSet.toArray();
         Road to_change = (Road) roadArray[id];
         Set<Cell> cellSet = to_change.getRoadGraph().vertexSet();
-        int length = to_change.getLength();
+        double length = to_change.getLength();
         Object[] cellArray = cellSet.toArray();
         int startIndex = (int) ((cellArray.length-2)*x/length);
         Cell cell = (Cell) cellArray[startIndex];
         cell.setMaxFlow(capacity/3600*timestep);
     }
 
-    public static void initializeFlow(Graph<Road, DefaultEdge> network) {
-        Set<Road> roadSet = network.vertexSet();
-        Object[] roadArray = roadSet.toArray();
-        for(int i = 1; i < roadArray.length; i++) {
-            Road road = (Road) roadArray[i];
-            Set<Cell> cellSet = road.getRoadGraph().vertexSet();
-            Object[] cellArray = cellSet.toArray();
-            for (int j = 0 ; j< cellArray.length-1;j++) {
-                Cell currentCell = (Cell) cellArray[j];
-                Cell nextCell = (Cell) cellArray[j+1];
-                if (j == 0) {
-                    currentCell.setCurrentFlow(nextCell.getNumCars());
-                    continue;
-                }
-                double S = Math.min(currentCell.getMaxFlow(),currentCell.getNumCars());
-                double R = Math.min(nextCell.getMaxFlow(),nextCell.getMaxCars()-nextCell.getNumCars());
-                double yt = Math.min(S,R);
-                currentCell.setCurrentFlow(yt);
-            }
-        }
-    }
 }
